4576f637673504b4852d55eced2a8cf89f7fd0fe
ThaiAnh <anh.nguyenthai@haravan.com>
2025-10-02 22:58:36 +0700

    Initial commit

---
+++ popzy.js
@@ -0,0 +1,229 @@
+
+Popzy.elements = [];
+
+function Popzy(options = {}) {
+    // const {
+    //     templateId,
+    //     destroyOnClose = true,
+    //     cssClass = [],
+    //     footer = false,
+    //     closeMethods = ["button", "overlay", "escape"],
+    //     onOpen,
+    //     onClose,
+    // } = options;
+
+    this.opt = Object.assign({
+        // templateId,
+        destroyOnClose: true,
+        cssClass: [],
+        footer: false,
+        closeMethods: ["button", "overlay", "escape"],
+        // onOpen,
+        // onClose,
+    }, options);
+
+    this.template = document.querySelector(`#${this.opt.templateId}`);
+    this._handleEscapeKey = this._handleEscapeKey.bind(this);
+
+    if (!this.template) {
+        console.error(`Template with id ${this.opt.templateId} not found`);
+        return;
+    };
+
+    const { closeMethods } = this.opt;
+    this._allowButtonClose = closeMethods.includes("button");
+    this._allowOverlayClose = closeMethods.includes("overlay");
+    this._allowEscapeClose = closeMethods.includes("escape");
+    this._footerButtons = [];
+}
+
+// Prototype
+Popzy.prototype._getScrollbarWidth = () => {
+    if (this._scrollbarWidth)
+        return this._scrollbarWidth;
+
+    const div = document.createElement("div");
+    Object.assign(div.style, {
+        overflow: "scroll",
+        position: "absolute",
+        top: "-9999px",
+    });
+
+    document.body.appendChild(div);
+    this._scrollbarWidth = div.offsetWidth - div.clientWidth;
+    document.body.removeChild(div);
+    return this._scrollbarWidth;
+}
+
+Popzy.prototype._build = function () {
+    /**
+     * Tại sao cần cloneNode(true)?
+     * 
+     * KHÔNG có cloneNode(true):
+     * - template.content được MOVE (không phải copy) vào modal
+     * - Lần 1: template.content → modal (hiển thị OK)
+     * - Đóng modal: template.content bị XÓA cùng modal
+     * - Lần 2: template.content đã RỖNG → modal trống
+     * 
+     * CÓ cloneNode(true):
+     * - Tạo bản COPY của template.content
+     * - Lần 1: copy → modal (template gốc vẫn còn)
+     * - Đóng modal: chỉ xóa bản copy
+     * - Lần 2: tạo copy mới → modal hiển thị bình thường
+     * 
+     * cloneNode(true) = deep clone (copy cả children elements)
+     */
+    const content = this.template.content.cloneNode(true);
+
+    // Create modal elements
+    this._backdrop = document.createElement("div");
+    this._backdrop.className = "popzy__backdrop";
+
+    const container = document.createElement("div");
+    container.className = "popzy__container";
+
+    this.opt.cssClass.forEach(className => {
+        if (typeof className === "string") {
+            container.classList.add(className);
+        }
+    });
+
+    if (this._allowButtonClose) {
+        const closeBtn = this._createButton("&times;", "popzy__close", () => this.close());
+        container.append(closeBtn);
+    }
+
+    const modalContent = document.createElement("div");
+    modalContent.className = "popzy__content";
+
+    // Append elements to the modal
+    modalContent.append(content);
+    container.append(modalContent);
+
+    if (this.opt.footer) {
+        this._modalFooter = document.createElement("div");
+        this._modalFooter.className = "popzy__footer";
+
+        this._renderFooterContent();
+        this._renderFooterButtons();
+
+        container.append(this._modalFooter);
+    }
+
+    this._backdrop.append(container);
+    document.body.append(this._backdrop);
+}
+
+Popzy.prototype.setFooterContent = function (html) {
+    this._footerContent = html;
+    this._renderFooterContent();
+};
+
+Popzy.prototype.addFooterButton = function (title, cssClass, callback) {
+    const button = this._createButton(title, cssClass, callback);
+    this._footerButtons.push(button);
+    this._renderFooterButtons();
+};
+
+Popzy.prototype._renderFooterContent = function () {
+    if (this._modalFooter && this._footerContent) {
+        this._modalFooter.innerHTML = this._footerContent;
+    }
+};
+
+Popzy.prototype._renderFooterButtons = function () {
+    if (this._modalFooter) {
+        this._footerButtons.forEach((button) => {
+            this._modalFooter.append(button);
+        });
+    }
+};
+
+Popzy.prototype._createButton = function (title, cssClass, callback) {
+    const button = document.createElement("button");
+    button.className = cssClass;
+    button.innerHTML = title;
+    button.onclick = callback;
+
+    return button;
+};
+
+Popzy.prototype.open = function () {
+    Popzy.elements.push(this);
+
+    if (!this._backdrop) {
+        this._build();
+    }
+
+    setTimeout(() => {
+        this._backdrop.classList.add("popzy--show");
+    }, 0);
+
+    // Disable scrolling
+    document.body.classList.add("popzy--no-scroll");
+    document.body.style.paddingRight = `${this._scrollbarWidth}px`;
+
+    // Attach event listeners
+    if (this._allowOverlayClose) {
+        this._backdrop.onclick = (e) => {
+            if (e.target === this._backdrop) {
+                this.close();
+            }
+        }
+    }
+
+    if (this._allowEscapeClose) {
+        document.addEventListener("keydown", this._handleEscapeKey);
+    }
+
+    this._onTransitionEnd(this.opt.onOpen);
+
+    return this._backdrop;
+}
+
+Popzy.prototype._handleEscapeKey = function (e) {
+    console.log(this);
+    const lastModal = Popzy.elements[Popzy.elements.length - 1];
+    if (e.key === "Escape" && this === lastModal) {
+        this.close();
+    }
+};
+
+Popzy.prototype._onTransitionEnd = function (callback) {
+    this._backdrop.ontransitionend = (e) => {
+        if (e.propertyName !== "transform") return;
+        if (typeof callback === "function") callback();
+    };
+};
+
+Popzy.prototype.close = function (destroy = this.opt.destroyOnClose) {
+    Popzy.elements.pop();
+
+    this._backdrop.classList.remove("popzy--show");
+
+    if (this._allowEscapeClose) {
+        document.removeEventListener("keydown", this._handleEscapeKey);
+    }
+
+    // Using transitionend event to wait for the animation in css to finish
+    // https://www.w3schools.com/jsref/event_transitionend.asp
+    this._onTransitionEnd(() => {
+        if (this._backdrop && destroy) {
+            this._backdrop.remove();
+            this._backdrop = null;
+            this._modalFooter = null;
+        };
+
+        // Enable scrolling
+        if (Popzy.elements.length === 0) {
+            document.body.classList.remove("popzy--no-scroll");
+            document.body.style.paddingRight = "";
+        }
+
+        if (typeof onClose === "function") this.opt.onClose();
+    });
+}
+
+Popzy.prototype.destroy = function () {
+    this.close(true);
+}
